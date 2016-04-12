0a8d83e01eb9db2273336c4883000d5e1f3c576f
Emilio Molina <emilio.mol.mar@gmail.com>
2016-04-12 03:09:30 +0200

    First working version

--- src/ecosystem.py
+++ src/ecosystem.py
@@ -1,5 +1,5 @@
 """
-Agent-based system emulating an ecosystem of virtual bugs eating each other
+Multi-agent system emulating an ecosystem of virtual bugs eating each other
 """
 import random
 
@@ -9,7 +9,7 @@ CARNIVORE = 'carnivore'
 
 
 class Ecosystem(object):
-    """ Conductor of the ecosystem
+    """ Environment of the ecosystem where organisms can live and evolve
 
     Role:
         - create and initialize organisms
@@ -19,16 +19,15 @@ class Ecosystem(object):
 
     def __init__(self):
         self.organisms_settings = {
-            'num_plants': 50,
-            'num_herbivores': 50,
-            'num_carnivores': 50
+            'num_plants': 40,
+            'num_herbivores': 20,
+            'num_carnivores': 10
         }
         self.biotope_settings = {
-            'size_x': 400,
-            'size_y': 400
+            'size_x': 200,
+            'size_y': 200
         }
         self.initialize_biotope()
-        self.organisms = {}
         self.initialize_organisms()
 
     def initialize_biotope(self):
@@ -36,7 +35,11 @@ class Ecosystem(object):
         """
         size_x = self.biotope_settings['size_x']
         size_y = self.biotope_settings['size_y']
-        self.biotope = [[None] * size_x] * size_y
+        self.biotope = {}
+        self.biotope_free_locs = set()
+        for x in range(0, size_x):
+            for y in range(0, size_y):
+                self.biotope_free_locs.add((x, y))
 
     def initialize_organisms(self):
         """ Method to initialize a set of organisms
@@ -47,27 +50,29 @@ class Ecosystem(object):
         num_herbivores = self.organisms_settings['num_herbivores']
         num_carnivores = self.organisms_settings['num_carnivores']
         id = 0
-
+        print "Creating plants..."
         # Create plants
         for i in range(0, num_plants):
             id += 1
-            random_location = self.seek_free_location()
+            random_location = random.sample(self.biotope_free_locs, 1)[0]
             self.add_organism(Organism(type=PLANT, parent_ecosystem=self,
-                                       location=random_location, id=id))
+                                       location=random_location))
 
+        print "Creating hervibores..."
         # Create hervibores
         for i in range(0, num_herbivores):
             id += 1
-            random_location = self.seek_free_location()
+            random_location = random.sample(self.biotope_free_locs, 1)[0]
             self.add_organism(Organism(type=HERBIVORE, parent_ecosystem=self,
-                                       location=random_location, id=id))
+                                       location=random_location))
 
+        print "Creating carnivores..."
         # Create carnivores
         for i in range(0, num_carnivores):
             id += 1
-            random_location = self.seek_free_location()
+            random_location = random.sample(self.biotope_free_locs, 1)[0]
             self.add_organism(Organism(type=CARNIVORE, parent_ecosystem=self,
-                                       location=random_location, id=id))
+                                       location=random_location))
 
     def add_organism(self, organism):
         """ Add organism to ecosytem
@@ -76,56 +81,90 @@ class Ecosystem(object):
             organism (Organism): Organism objects to be added to ecosystem
             id (int): Id assigned to organism
         """
-        self.organisms[organism.id] = organism
         (x, y) = organism.location
-        self.biotope[x][y] = organism.id
+        self.biotope[(x, y)] = organism
+        self.biotope_free_locs.remove((x, y))
 
     def delete_organism(self, organism):
         """ Remove organism from ecosysem
+
+        Note: Organisms objects are not deleted manually. We rely on
+              Python garbage collector.
 
         Args:
             organism (Organism): Organism to be removed from ecosystem
         """
         (x, y) = organism.location
-        del self.organisms[organism.id]
-        self.biotope[x][y] = None
+        del self.biotope[(x, y)]  # delete reference from dict
+        self.biotope_free_locs.add((x, y))
 
-    def seek_free_location(self, reference_location=None):
-        """ Seek a free location in biotope
-
-        If reference_location parameter is specified, the closest free location
-        to it is returned. If several equally close free locations are found,
-        it is selected randomly among them.
-
+    def update_organism_location(self, organism):
+        """ Update interval variables related to a given organism
 
         Args:
-            close_to (Optional[tuple]): Tuple with (x, y) coordinates of
-                                        reference location.
-        Returns:
-            (tuple): (x, y) coordinates of a free location. Is not available,
-                     returns None.
+            organism (Organism): Organism to be removed from ecosystem
         """
-        # First finds free locations
-        # TODO: make this list global and update it when needed.
-        free_locations = []
-        for x in range(0, len(self.biotope)):
-            for y in range(0, len(self.biotope[x])):
-                if reference_location is None:
-                    d = None
-                else:
-                    (ref_x, ref_y) = reference_location
-                    d = abs(ref_x - x) + abs(ref_y - y)  # Manhattan distance
-                if self.biotope[x][y] is None:
-                    free_locations.append((x, y, d))
-        # Then shuffle or order list of free locations
-        random.shuffle(free_locations)
-        if reference_location is None:
-            free_location = free_locations[0]
-            return (free_location[0], free_location[1])  # (x, y)
-        else:
-            free_locations.sort(key=lambda x: x[2])
-            free_location = free_locations[0]
-            return (free_location[0], free_location[1])  # (x, y)
+        old_location = organism.old_location
+        del self.biotope[old_location]
+        self.biotope_free_locs.add(old_location)
+
+        new_location = organism.location
+        self.biotope[new_location] = organism
+        self.biotope_free_locs.remove(new_location)
+
+        organism.old_location = organism.location
+
+    def get_surrounding_free_locations(self, center):
+        """ Get a list of free locations around a given center
+
+        Args:
+            center (tuple): (x, y) coordinates of center
+        Returns:
+            (set): Set of free locations around center
+        """
+        (center_x, center_y) = center
+        surrounding_free_locs = set()
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if (dx == 0) and (dy == 0):
+                    break  # avoid checking center itself
+                x = center_x + dx
+                y = center_y + dy
+                if (x, y) in self.biotope_free_locs:
+                    surrounding_free_locs.add((x, y))
+        return surrounding_free_locs
+
+    def get_surrounding_organisms(self, center):
+        """ Get a list of organisms around a given center
+
+        Args:
+            center (tuple): (x, y) coordinates of center
+        Returns:
+            (set): Set of organisms
+        """
+        (center_x, center_y) = center
+        surrounding_organisms = set()
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if (dx == 0) and (dy == 0):
+                    break  # avoid checking center itself
+                x = center_x + dx
+                y = center_y + dy
+                if (x, y) in self.biotope.keys():
+                    organism = self.biotope[(x, y)]
+                    surrounding_organisms.add(organism)
+        return surrounding_organisms
+
+    def evolve(self):
+        """ Evolve one time step the whole ecosystem
+        """
+        curr_organisms = self.biotope.values()
+        for organism in curr_organisms:
+            if organism.is_alive:  # still alive
+                organism.do_move()
+                organism.do_hunt()
+                organism.do_procreate_if_possible()
+                organism.do_age()
 
 
 class Organism(object):
@@ -140,80 +179,139 @@ class Organism(object):
             parent_ecosystem (Ecosystem): Parent ecosystem
             location (tuple): (x, y) coordinates of organism
         """
-        DEFAULT_AGE = 500
-        self.type = type
+        # Relative to ecosystem
         self.parent_ecosystem = parent_ecosystem
         self.location = location
-        self.age = DEFAULT_AGE
-        self.alive = True
+        self.old_location = location  # useful for parent_ecosystem to track it
+        # Genes and state
+        self.type = type
+        self.death_age = random.randint(0, 500)  # TODO: Parametrize
+        if type == PLANT:
+            self.procreation_prob = 0.98
+        elif type == HERBIVORE:
+            self.procreation_prob = 0.99
+        elif type == CARNIVORE:
+            self.procreation_prob = 0.995
+        self.age = 0
+        self.is_alive = True
 
-    def move(self):
-        """ Move organism to a free location in ecosystem
+    def do_age(self):
+        """ Increase self.age and die if reach self.death_age
         """
-        self.location = self.parent_ecosystem.seek_free_location(
-            self.location)
+        self.age += 1
+        if self.age == self.death_age:
+            self.do_die()
 
-    def is_eatable(self, target_organism):
-        """ Returns true if target_organism can be eaten by self
+    def do_move(self):
+        """ Move organism to a free location in ecosystem
+
+        Returns:
+            (x, y): New location
+        """
+        if self.type == PLANT:  # Plants don't move
+            return self.location
+
+        free_locs = self.parent_ecosystem.get_surrounding_free_locations(
+            self.location)
+        if len(free_locs) > 0:
+            new_location = random.sample(free_locs, 1)[0]
+            self.location = new_location
+        self.parent_ecosystem.update_organism_location(self)
+        return new_location
+
+    def do_die(self):
+        """ Make organism dissapear from ecosystem
+        """
+        self.is_alive = False
+        self.parent_ecosystem.delete_organism(self)
+
+    def is_eatable(self, pray):
+        """ True if pray can be eaten by self
 
         Args:
-            target_organism (Organism): Organism to be eaten
+            pray (Organism): Organism to be eaten
+        Returns:
+            (bool): True if pray can be eaten
         """
         eatable = False
-        if self.type == CARNIVORE and target_organism.type == HERBIVORE:
+        if self.type == CARNIVORE and pray.type == HERBIVORE:
             eatable = True
-        if self.type == HERBIVORE and target_organism.type == PLANT:
+        if self.type == HERBIVORE and pray.type == PLANT:
             eatable = True
         return eatable
 
-    def eat(self):
-        """ Check if some food is around, and eat it.
-
-        In this first version, the predator 'sucks' the pray's age and kill it.
+    def do_hunt(self):
+        """ Find food nearby and eat it
         """
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                if (dx == 0) and (dy == 0):
-                    break  # can not eat itself
-                target_x = self.location[0] + dx
-                target_y = self.location[1] + dy
-                target_organism_id = \
-                    self.parent_ecosystem.biotope[target_x][target_y]
+        surr_organisms = self.parent_ecosystem.get_surrounding_organisms(
+            self.location)
+        for surr_organism in surr_organisms:
+            if self.is_eatable(surr_organism):
+                pray = surr_organism
+                self.age = 0  # The benefit of eating is getting younger
+                pray.do_die()
+                break
 
-                if target_organism_id is not None:
-                    target_organism = \
-                        self.parent_ecosystem.organisms[target_organism_id]
-
-                if self.is_eatable(target_organism):
-                    # Bingo! It is food!
-                    self.age += target_organism.age
-                    target_organism.kill()
-
-    def kill(self):
-        """ Kill organism (e.g. when it is eaten)
+    def do_procreate_if_possible(self):
+        """ Procreate if possible
         """
-        self.alive = False
+        free_locs = self.parent_ecosystem.get_surrounding_free_locations(
+            self.location)
+        if len(free_locs) == 0:
+            return  # no empty space for procreation!
+        baby_location = random.sample(free_locs, 1)[0]
+
+        # Procreate if desired
+        if random.random() > self.procreation_prob:
+            baby = Organism(self.type, self.parent_ecosystem, baby_location)
+            self.parent_ecosystem.add_organism(baby)
 
 
 class GUI(object):
-    pass
+    """ Class for drawing an ecosystem
+    """
+
+    def __init__(self, parent_ecosystem):
+        """ Initializes GUI
+        """
+        import matplotlib.pyplot as plt
+        self.parent_ecosystem = parent_ecosystem
+        self.img = plt.imshow([[0.0]], interpolation='nearest')
+        plt.ion()
+        plt.show()
+
+    def draw_ecosystem(self):
+        """ Draw ecosystem using Matplotlib
+        """
+        import matplotlib.pyplot as plt
+        BGCOLOR = (0, 0, 0)
+        color_mapping = {
+            PLANT: (0, 0.75, 0),
+            HERBIVORE: (0.5, 0.5, 0.5),
+            CARNIVORE: (0.75, 0, 0)
+        }
+        size_x = self.parent_ecosystem.biotope_settings['size_x']
+        size_y = self.parent_ecosystem.biotope_settings['size_y']
+        pixels_map = [[BGCOLOR for i in range(size_x)]
+                      for j in range(size_y)]
+
+        for location, organism in self.parent_ecosystem.biotope.iteritems():
+            (x, y) = location
+            pixels_map[x][y] = color_mapping[organism.type]
+
+        self.img.set_data(pixels_map)
+        plt.pause(0.01)  # needed to avoid overloading of GUI
 
 
 def main():
     """ Main function of ecosystem
     """
-    import time
-    step_time = 1.0
     ecosystem = Ecosystem()
     gui = GUI(ecosystem)
 
     while True:
-        ecosystem.evolve()
         gui.draw_ecosystem()
-        if step_time > 0.0:
-            time.sleep(step_time)
+        ecosystem.evolve()
 
 if __name__ == '__main__':
-    """ Code to be run if script is run from commandline
-    """
     main()
