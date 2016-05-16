8eec51ec41823f3b1c7b700b951340657934d82c
Emilio Molina <emilio.mol.mar@gmail.com>
2016-05-16 19:02:55 +0200

    Add new files

---
+++ src/cpp_version/gui_juce/Source/ExperimentComponent.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ExperimentComponent.h
+    Created: 16 May 2016 6:46:19pm
+    Author:  Emilio Molina
+
+  ==============================================================================
+*/
+
+#ifndef EXPERIMENTCOMPONENT_H_INCLUDED
+#define EXPERIMENTCOMPONENT_H_INCLUDED
+
+
+
+
+
+#endif  // EXPERIMENTCOMPONENT_H_INCLUDED
---
+++ src/cpp_version/gui_juce/Source/MapComponent.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    MapComponent.h
+    Created: 16 May 2016 6:46:10pm
+    Author:  Emilio Molina
+
+  ==============================================================================
+*/
+
+#ifndef MAPCOMPONENT_H_INCLUDED
+#define MAPCOMPONENT_H_INCLUDED
+
+
+
+
+
+#endif  // MAPCOMPONENT_H_INCLUDED
