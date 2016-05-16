f57026f21f4feae808ee684ed362d9c198b4a089
Emilio Molina <emilio.mol.mar@gmail.com>
2016-05-16 12:26:55 +0200

    Draft ExperimentFolderInterface class

--- src/cpp_version/gui_juce/Source/MainComponent.cpp
+++ src/cpp_version/gui_juce/Source/MainComponent.cpp
@@ -1,5 +1,7 @@
 #include "MainComponent.h"
 namespace bf=boost::filesystem;
+using json = nlohmann::json;
+
 
 CriticalSection mtx;
 
@@ -304,6 +306,96 @@ private:
     String newVertexShader, newFragmentShader;
 };
 
+
+/** @brief Class to easily interact with experiment folder
+*/
+class ExperimentFolderInterface {
+    
+    /** @brief Initializer
+    *
+    * @param[in] experiment_folder String with experiment_folder path
+    */
+    ExperimentFolderInterface(string experiment_folder);
+
+
+    /** @brief Get a list of time slices containing a complete backup of ecosystem
+    *
+    * @returns List of time slices allowing complete backup
+    */
+    vector<int> getTimesHavingCompleteBackups();
+    
+
+    /** @brief Get path of JSON containing ecosystem data
+    *
+    * @param[in] time_slice Time slice for which ecosystem data will be get
+    *
+    * @returns Path of JSON file
+    */
+    string getEcosystemJSONPath(int time_slice);
+    
+    /** @brief Get a list of time slices containing a given organism parameter
+    *
+    * @param[in] parameter Parameter to read from JSON files
+    *
+    * @returns Vector of time values
+    */
+    vector<int> getTimesHavingOrganismParameter(string parameter);
+    
+    
+    /** @brief Get a list of time slices containing a given ecosystem parameter
+    *
+    * @param[in] parameter Parameter to read from JSON files
+    *
+    * @returns Vector of time values
+    */
+    vector<int> getTimesHavingEcosystemParameter(string parameter);
+    
+
+    /** @brief Get a list of available ecosystem parameters
+     *
+     * @returns Vector of parameters available
+     */
+    vector<string> getAvailableEcosystemParameters();
+
+    
+    /** @brief Get a list of available organism parameters
+    *
+    * @returns Vector of parameters available
+    */
+    vector<string> getAvailableOrganismParameters();
+    
+    /** @brief Get ecosystem data for a given time slice
+    *
+    * @param[in] time_slice Target time slice
+    */
+    json getEcosystemData(int time_slice);
+    
+    /** @brief Get ecosystem data for a given time slice
+    *
+    * @param[in] organism_id Organism ID
+    * @param[in] time_slice Target time slice
+    */
+    json getOrganismData(int organism_id, int time_slice);
+    
+    
+    /** @brief Get a list of avaiable living organisms ids
+     *
+     * @param[in] time_slice Target time slice
+     */
+    vector<int> getLivingOrganismsIds(int time_slice);
+    
+    /** @brief Get a list of avaiable dead organisms ids
+     *
+     * @param[in] time_slice Target time slice
+     */
+    vector<int> getDeadOrganismsIds(int time_slice);
+    
+    /* @brief Get experiment size in MBs in format e.g. "321.16MB"
+     *
+     * @returns String with experiment size
+     */
+    string getExperimentSize();
+};
 
 /** @brief Component for experiment tab
 *
