dc47a0286bbb3f0e7374116beb8036900f2aa195
Emilio Molina <emilio.mol.mar@gmail.com>
2016-05-16 21:46:03 +0200

    Experiment interface to new file

---
+++ src/cpp_version/ExperimentInterface.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ExperimentInterface.h
+    Created: 16 May 2016 9:45:31pm
+    Author:  Emilio Molina
+
+  ==============================================================================
+*/
+
+#ifndef EXPERIMENTINTERFACE_H_INCLUDED
+#define EXPERIMENTINTERFACE_H_INCLUDED
+
+
+
+
+
+#endif  // EXPERIMENTINTERFACE_H_INCLUDED
